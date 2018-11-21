6725d6415d0025c476c7b69fd9d9c9dc40bfe4eb
unknown <vglipyanin@edu.hse.ru>
2018-11-22 01:28:18 +0300

    DAOImpl without update()

--- src/main/Java/com/netcracker/edu/dao/EmployeeDAOImpl.java
+++ src/main/Java/com/netcracker/edu/dao/EmployeeDAOImpl.java
@@ -22,19 +22,30 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     }
 
     public Employee getEmployee(int key) {
-        return null;
+        try {
+            Employee employee = employees.get(key);
+            logger.info("Employee () with key () was found", employee.getFullName(), key);
+            return employee;
+        } catch (IndexOutOfBoundsException ex){
+            logger.warn("Key () is out of bounds", key);
+            return null;
+        }
     }
 
     public void updateEmployee(Employee employee) {
+        if(employee == null)
+            return;
 
+        employees.indexOf(employee);
     }
 
     public void deleteEmployee(Employee employee) {
-        try {
-            employees.remove(employee);
-            logger.info("() was successfully removed", employee.toString());
-        } catch (NullPointerException ex){
+        if(employee == null)
+            return;
 
-        }
+        if (employees.remove(employee))
+            logger.info("() was successfully removed", employee.getFullName());
+        else
+            logger.warn("() wasn't removed", employee.getFullName());
     }
 }
